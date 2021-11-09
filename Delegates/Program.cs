84343cdd04d93fbbf84da3c045d3ea013c925d1c
Pedro Torres <pedrojuantorres.info@gmail.com>
2021-11-09 11:25:51 +0000

    Solve Ex02

---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Joiner joinObject = new Joiner("Para");
+
+            MyDelegate stringFormat = PrintUpper;
+            stringFormat += PrintLower;
+            stringFormat += joinObject.JoinAndPrint;
+
+            stringFormat("Teste");
+            
+        }
+
+        public static void PrintUpper(string str) 
+        {
+            Console.WriteLine( str.ToUpper());
+        }
+        public static void PrintLower(string str) 
+        {
+            Console.WriteLine( str.ToLower());
+        }
+    }
+}
